5e9a2a980b565ddcfeba17ed1462d5a50813af41
ouhao <ouhao@ouxiaohao.com>
2017-06-25 23:58:35 +0800

    整合函数

--- config/function.php
+++ config/function.php
@@ -266,3 +266,62 @@ function sldc_express($param){
     $result = object_to_array($result);
     return $result;
 }
+//将数字转换成大写字母
+function numtostr($num)
+{
+    $arr = array(0 => 'A', 1 => 'B', 2 => 'C', 3 => 'D', 4 => 'E', 5 => 'F', 6 => 'G', 7 => 'H', 8 => 'I', 9 => 'J', 10 => 'K', 11 => 'L', 12 => 'M', 13 => 'N', 14 => 'O', 15 => 'P', 16 => 'Q', 17 => 'R', 18 => 'S', 19 => 'T', 20 => 'U', 21 => 'V', 22 => 'W', 23 => 'X', 24 => 'Y', 25 => 'Z');
+    return $arr[$num];
+}
+//搜索关键字标红
+function returnred($key, $content)
+{
+    return str_replace($key, '<font color="red">' . $key . '</font>', $content);
+}
+/**
+ * 获取首字母
+ */
+if (!function_exists('getfirstchar')) {
+    function getfirstchar($s0)
+    {
+        $firstchar_ord = ord(strtoupper($s0{0}));
+        if (($firstchar_ord >= 65 and $firstchar_ord <= 91) or ($firstchar_ord >= 48 and $firstchar_ord <= 57)) return $s0{0};
+        $s = iconv("UTF-8", "gb2312", $s0);
+        $asc = ord($s{0}) * 256 + ord($s{1}) - 65536;
+        if ($asc >= -20319 and $asc <= -20284) return "A";
+        if ($asc >= -20283 and $asc <= -19776) return "B";
+        if ($asc >= -19775 and $asc <= -19219) return "C";
+        if ($asc >= -19218 and $asc <= -18711) return "D";
+        if ($asc >= -18710 and $asc <= -18527) return "E";
+        if ($asc >= -18526 and $asc <= -18240) return "F";
+        if ($asc >= -18239 and $asc <= -17923) return "G";
+        if ($asc >= -17922 and $asc <= -17418) return "H";
+        if ($asc >= -17417 and $asc <= -16475) return "J";
+        if ($asc >= -16474 and $asc <= -16213) return "K";
+        if ($asc >= -16212 and $asc <= -15641) return "L";
+        if ($asc >= -15640 and $asc <= -15166) return "M";
+        if ($asc >= -15165 and $asc <= -14923) return "N";
+        if ($asc >= -14922 and $asc <= -14915) return "O";
+        if ($asc >= -14914 and $asc <= -14631) return "P";
+        if ($asc >= -14630 and $asc <= -14150) return "Q";
+        if ($asc >= -14149 and $asc <= -14091) return "R";
+        if ($asc >= -14090 and $asc <= -13319) return "S";
+        if ($asc >= -13318 and $asc <= -12839) return "T";
+        if ($asc >= -12838 and $asc <= -12557) return "W";
+        if ($asc >= -12556 and $asc <= -11848) return "X";
+        if ($asc >= -11847 and $asc <= -11056) return "Y";
+        if ($asc >= -11055 and $asc <= -10247) return "Z";
+        return null;
+    }
+}
+/**
+ * 去除数组俩表的空格
+ */
+function array_map_trim($v)
+{
+    if(!is_array($v)){
+        return trim($v);
+    }else{
+        return $v;
+    }
+
+}
